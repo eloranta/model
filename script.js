99a5ab5f1fac81a4138f4e732a3e6a893ad2ca05
Hannu Eloranta <hannu.eloranta@gmail.com>
2015-04-09 16:00:05 +0300

    Refactor: simplified javascript

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 var app = angular.module('app',[]);
 
 app.controller('controller', function($scope) {
-  $scope.values = new Array(40); 
+  $scope.values = new Array(40);
   for (i = 0; i < $scope.values.length; i++) {
-    var val = Math.floor((Math.random() * 2));
-    if (val === 0) {
-      $scope.values[i] = false;
-    } else {
-      $scope.values[i] = true;
-    }
+    $scope.values[i] = Math.random()<0.5;
   }    
 });
  
