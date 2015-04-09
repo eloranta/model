96231fe689c73735885885b5fd3dc239436575c0
Hannu Eloranta <hannu.eloranta@gmail.com>
2015-04-09 10:35:29 +0300

    Adding a controller which takes care of initialization and toggling of the state

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-angular.module('app',[]);
+var app = angular.module('app',[]);
+
+app.controller('controller', function($scope) {
+    $scope.state = true;
+    $scope.toggle = function() {
+      $scope.state = !$scope.state;
+  };
+ });
+ 
