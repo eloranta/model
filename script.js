b6e7b04064eb525a6f448b33c9089f85f283ff7d
Hannu Eloranta <hannu.eloranta@gmail.com>
2015-04-09 15:30:03 +0300

    40 separate boolean values

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 var app = angular.module('app',[]);
 
-app.controller('controller', function() {
-    this.value1 = true;
-    this.value2 = false;
-    this.toggle = function(item) {
-      if (item===1 || item===3) {
-        this.value1 = !this.value1;
-      }
-      if (item===2 || item===3) {
-        this.value2 = !this.value2;
-      }
-  };
- });
+app.controller('controller', function($scope) {
+  $scope.values = new Array(40); 
+  for (i = 0; i < $scope.values.length; i++) {
+    var val = Math.floor((Math.random() * 2));
+    if (val === 0) {
+      $scope.values[i] = false;
+    } else {
+      $scope.values[i] = true;
+    }
+  }    
+});
  
