ea309c7d065a44bea21d3e3b8763b81719f10302
Hannu Eloranta <hannu.eloranta@gmail.com>
2015-04-09 10:50:58 +0300

    Model is two boolean values

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 var app = angular.module('app',[]);
 
 app.controller('controller', function() {
-    this.value = true;
-    this.toggle = function() {
-      this.value = !this.value;
+    this.value1 = true;
+    this.value2 = false;
+    this.toggle = function(item) {
+      if (item===1 || item===3) {
+        this.value1 = !this.value1;
+      }
+      if (item===2 || item===3) {
+        this.value2 = !this.value2;
+      }
   };
  });
  
